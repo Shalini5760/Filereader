fa0a5fb1401690790001bcd5769d339d37bccca6
Shalini5760 <shalinie162@gmail.com>
2025-06-27 19:13:08 +0530

    Add files via upload

---
+++ filereader.java
@@ -0,0 +1,19 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileBasic {
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("example.txt");
+            int ch;
+
+            while ((ch = reader.read()) != -1) {
+                System.out.print((char) ch);
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
+    }
+}
